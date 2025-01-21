082a494e40d445427351f8b347c5ff31319f5117
1blezzed1 <143650732+1blezzed1@users.noreply.github.com>
2025-01-22 01:25:28 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -2760,8 +2760,7 @@ do
 
 
 
-    local KeybindOuter = Library:Create('ImageLabel', {
-        Image = 'http://www.roblox.com/asset/?id=139835207386022';
+    local KeybindOuter = Library:Create('Frame', {
         AnchorPoint = Vector2.new(0, 0.5);
         BorderColor3 = Color3.new(0, 0, 0);
         Position = UDim2.new(0, 10, 0.5, 0);
